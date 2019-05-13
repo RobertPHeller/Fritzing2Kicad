f980abd1007ff386be39763dd59b50ed19942e54
Robert Heller <heller@deepsoft.com>
2019-05-12 21:05:30 -0400

    Initial bare Build Infrastructure

---
+++ BuildScripts/tcldoxyfilter.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/home/heller/FritzingPartsSVGEditor/BuildData/tclkit /home/heller/FritzingPartsSVGEditor/BuildScripts/tcl2dox.kit $*
---
+++ ExtraDoc/Version.tcl
@@ -0,0 +1,11 @@
+## @page Version Version
+# Version is 0.0.2 on x86_64-unknown-linux-gnu.
+# Built on Sat May 11 11:14:52 EDT 2019, build system: gollum.deepsoft.com x86_64
+#
+
+namespace eval Version {
+    variable VERSION {0.0.2}
+    variable target  {x86_64-unknown-linux-gnu}
+    variable build   {201905111114-gollum.deepsoft.com-x86_64}
+}
+package provide Version 0.0.2
